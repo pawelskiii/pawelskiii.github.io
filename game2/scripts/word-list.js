92a88eec3b6983978bd0636edc5b78c7d9329af9
Pawel Racki <pracki@atlassian.com>
2024-08-28 11:13:27 +0200

    initial commit4

---
+++ game2/scripts/word-list.js
@@ -0,0 +1,22 @@
+const wordList = [
+  {
+    word: "zielony",
+    hint: "Najlepszy kolor w grze Magic: The Gathering.",
+  },
+  {
+    word: "sanitariusz",
+    hint: "IO IO IO IO. Główny pracownik pogotowia alkoholowego - Mariusz ...",
+  },
+  {
+    word: "wojownicy",
+    hint: "Zawsze gotowi do boju. Lubują się w trunkach wysokoprocentowych, a przede wszystkim w wódce.",
+  },
+  {
+    word: "koszula",
+    hint: '"... w kratę, masz fajnego tatę."',
+  },
+  {
+    word: "ryzykant",
+    hint: "Tytuł który zyskał Ryś swoimi szalonymi akcjami.",
+  },
+];
---
+++ game4/script.js
@@ -0,0 +1,85 @@
+// Get  to DOM elements
+const gameContainer = document.querySelector(".container"),
+  userResult = document.querySelector(".user_result img"),
+  cpuResult = document.querySelector(".cpu_result img"),
+  result = document.querySelector(".result"),
+  optionImages = document.querySelectorAll(".option_image"),
+  score = document.querySelector(".score b"),
+  nextLevel = document.querySelector(".hidden");
+
+let wins = 0;
+// Loop through each option image element
+optionImages.forEach((image, index) => {
+  image.addEventListener("click", (e) => {
+    image.classList.add("active");
+
+    userResult.src = cpuResult.src = "images/rock.png";
+    result.textContent = "Czekaj...";
+
+    // Loop through each option image again
+    optionImages.forEach((image2, index2) => {
+      // If the current index doesn't match the clicked index
+      // Remove the "active" class from the other option images
+      index !== index2 && image2.classList.remove("active");
+    });
+
+    gameContainer.classList.add("start");
+
+    // Set a timeout to delay the result calculation
+    let time = setTimeout(() => {
+      gameContainer.classList.remove("start");
+
+      // Get the source of the clicked option image
+      let imageSrc = e.target.querySelector("img").src;
+      // Set the user image to the clicked option image
+      userResult.src = imageSrc;
+
+      // Generate a random number between 0 and 2
+      let randomNumber = Math.floor(Math.random() * 3);
+      // Create an array of CPU image options
+      let cpuImages = [
+        "images/rock.png",
+        "images/paper.png",
+        "images/scissors.png",
+      ];
+      // Set the CPU image to a random option from the array
+      cpuResult.src = cpuImages[randomNumber];
+
+      // Assign a letter value to the CPU option (R for rock, P for paper, S for scissors)
+      let cpuValue = ["R", "P", "S"][randomNumber];
+      // Assign a letter value to the clicked option (based on index)
+      let userValue = ["R", "P", "S"][index];
+
+      // Create an object with all possible outcomes
+      let outcomes = {
+        RR: "Draw",
+        RP: "Ryś",
+        RS: "Łysy",
+        PP: "Draw",
+        PR: "Łysy",
+        PS: "Ryś",
+        SS: "Draw",
+        SR: "Ryś",
+        SP: "Łysy",
+      };
+
+      // Look up the outcome value based on user and CPU options
+      let outComeValue = outcomes[userValue + cpuValue];
+
+      // Display the result
+      result.textContent =
+        userValue === cpuValue ? "Remis" : `${outComeValue} Wygrał!!`;
+      if (userValue !== cpuValue && outComeValue === "Łysy") {
+        wins++;
+        if (wins >= 3) {
+          nextLevel?.classList.remove("hidden");
+        }
+        if (wins <= 3) {
+          score.innerText = `${wins} / 3`;
+        } else {
+          score.innerText = `dobra, starczy tego grania`;
+        }
+      }
+    }, 2000);
+  });
+});
